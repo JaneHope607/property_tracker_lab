8e6fe2097ae14713e1b919424402a944cb3bb199
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-06-30 14:54:51 +0100

    save method added

--- db/estate_agents.sql
+++ db/estate_agents.sql
@@ -4,6 +4,6 @@ CREATE TABLE properties(
     id SERIAL PRIMARY KEY,
     address VARCHAR(255),
     value INT,
-    year_built INT,
+    year_built INT,  
     build VARCHAR(255)
 );
--- models/property.rb
+++ models/property.rb
@@ -8,9 +8,23 @@ class Property
     def initialize(options)
         @id = options['id'].to_i() if options['id']
         @address = options['address']
-        @value = options['value']
-        @year_built = options['year_built']
+        @value = options['value'].to_i()
+        @year_built = options['year_built'].to_i()
         @build = options['build']
     end
+
+    def save()
+        db = PG.connect( {dbname: 'estate_agents', host: 'localhost'} )
+        sql = "INSERT INTO properties
+        (address, value, year_built, build)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id"
+        values = [@address, @value, @year_built, @build]
+        db.prepare("save", sql)
+        @id = db.exec_prepared("save", values)[0]['id'].to_i()
+        db.close()
+    end
+
+
 
 end

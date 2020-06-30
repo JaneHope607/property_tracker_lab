9f939fc956cd95c252c2b615de283da349b107b0
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-06-30 15:22:12 +0100

    property 2 instance created and delete method passes

--- console.rb
+++ console.rb
@@ -6,8 +6,13 @@ Property.delete_all()
 property1 = Property.new( {'address' => '15 Braid Hills Road', 'value' => 400000, 'year_built' => 2000, 'build' => 'semi-detached'} )
 property1.save()
 
+property2 = Property.new( { 'address' => '10 Belmont Crescent', 'value' => 250000, 'year_built' => 1895, 'build' => 'terrace'})
+property2.save()
+
 property1.value = 420000
 property1.update()
+
+property2.delete()
 
 binding.pry
 nil
